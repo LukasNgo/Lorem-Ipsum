0e7bd78659a7152df805975b4bcd174238cb0d00
Lukas Ngo <32539108+LukasNgo@users.noreply.github.com>
2018-02-21 15:16:59 +0000

    Setup
    setup release done

---
+++ LoremIpsum/Assets/Scripts/MenuToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject menu;
+
+    private bool menuState = false;
+
+    private void OnEnable()
+    {
+        controllerEvents.ButtonTwoPressed += controllerEvents_ButtonTwoPressed;
+    }
+
+    private void OnDisable()
+    {
+        controllerEvents.ButtonTwoPressed -= controllerEvents_ButtonTwoPressed;
+    }
+
+    private void controllerEvents_ButtonTwoPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        ChangeState();
+    }
+
+    public void ChangeState()
+    {
+        menuState = !menuState;
+        menu.SetActive(menuState);
+        GetComponent<VRTK_Pointer>().enableTeleport = !menuState;
+    }
+}
