42c55b4b01271c4ae9ecabdde7e5f446b49aa14d
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-02-24 19:15:27 +0000

    Placeobject
    created a colored object of the placeable version at the pointer's end.

--- LoremIpsum/Assets/Scripts/PlaceObjects.cs
+++ LoremIpsum/Assets/Scripts/PlaceObjects.cs
@@ -7,7 +7,22 @@ public class PlaceObjects : MonoBehaviour {
 
     public VRTK_ControllerEvents controllerEvents;
     public VRTK_BasePointerRenderer pointerRenderer;
-    public GameObject SelectedObject;
+
+    public GameObject SelectedObject = null;//Change to private later
+    private GameObject ShowVerison;
+    private Vector3 placeLocation;
+
+    private void Start()
+    {
+        ShowVerison = Instantiate(SelectedObject, transform);
+
+        ShowVerison.GetComponent<Collider>().enabled = false;
+        for (int i = 0; i < ShowVerison.GetComponentsInChildren<Collider>().Length; i++)
+        {
+            ShowVerison.GetComponentsInChildren<Collider>()[i].enabled = false;
+        }
+        //comment out later as it should start out null
+    }
 
     private void OnEnable()
     {
@@ -21,26 +36,49 @@ public class PlaceObjects : MonoBehaviour {
 
     private void controllerEvents_TriggerReleased(object sender, ControllerInteractionEventArgs e)
     {
-        PlaceObject();
+        if ((SelectedObject != null) && (pointerRenderer.IsVisible() == true) && (pointerRenderer.IsValidCollision() == true))
+        {
+            PlaceObject();
+        }
     }
 
     //Below are the meaty scripts
 
     private void PlaceObject()
     {
-        Vector3 placeLocation = pointerRenderer.GetDestinationHit().point;
-
-        Instantiate(SelectedObject, placeLocation, Quaternion.identity);
+        Instantiate(SelectedObject, placeLocation, Quaternion.identity);//Replace Instantiate with an object pool
     }
 
     private void Update()
     {
-        
+        placeLocation = pointerRenderer.GetDestinationHit().point;
+
+        if ((pointerRenderer.IsVisible() == true) && (SelectedObject != null) && (pointerRenderer.IsValidCollision() == true))
+        {
+            ShowVerison.SetActive(true);
+            ShowVerison.transform.position = placeLocation;
+            ShowVerison.GetComponent<Renderer>().material.color = pointerRenderer.validCollisionColor;
+        }
+        else
+        {
+            ShowVerison.SetActive(false);
+        }
     }
 
     //Used to changed the gameobject selected
     public void ChangeSelectedObject(GameObject newObject)
     {
         SelectedObject = newObject;
+
+        if (SelectedObject != null)
+        {
+            ShowVerison = Instantiate(SelectedObject, placeLocation, Quaternion.identity);
+            ShowVerison.GetComponent<Collider>().enabled = false;
+
+            for (int i = 0; i < ShowVerison.GetComponentsInChildren<Collider>().Length; i++)
+            {
+                ShowVerison.GetComponentsInChildren<Collider>()[i].enabled = false;
+            }
+        }
     }
 }
