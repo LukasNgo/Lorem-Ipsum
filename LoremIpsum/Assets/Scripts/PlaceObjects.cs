da40e9075d46fa2ee1a0178725d6d0ad853bbe39
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-02-24 18:04:47 +0000

    Place Object Start
    Objects can now be placed using the right build controller.  Releasing the trigger will place a box.  There is a method that can be used to swap the currently placeable object.

---
+++ LoremIpsum/Assets/Scripts/PlaceObjects.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlaceObjects : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+    public VRTK_BasePointerRenderer pointerRenderer;
+    public GameObject SelectedObject;
+
+    private void OnEnable()
+    {
+        controllerEvents.TriggerReleased += controllerEvents_TriggerReleased;
+    }
+
+    private void OnDisable()
+    {
+        controllerEvents.TriggerReleased -= controllerEvents_TriggerReleased;
+    }
+
+    private void controllerEvents_TriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        PlaceObject();
+    }
+
+    //Below are the meaty scripts
+
+    private void PlaceObject()
+    {
+        Vector3 placeLocation = pointerRenderer.GetDestinationHit().point;
+
+        Instantiate(SelectedObject, placeLocation, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    //Used to changed the gameobject selected
+    public void ChangeSelectedObject(GameObject newObject)
+    {
+        SelectedObject = newObject;
+    }
+}
