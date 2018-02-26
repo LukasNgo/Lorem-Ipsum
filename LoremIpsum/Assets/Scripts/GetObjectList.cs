8354b3228ab7883da56f1a4df37b688b0520cb19
DevLiamW <liam.west@students.plymouth.ac.uk>
2018-02-26 14:33:39 +0000

    Object Array, Object Array get Test
    + Object Array
    + Serializable field for object array
    + Getter for array
    + Test for geting array

---
+++ LoremIpsum/Assets/Scripts/GetObjectList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetObjectList : MonoBehaviour {
+
+    private ObjectList objectListScript;
+
+    private GameObject[] objectListArray;
+
+	// Use this for initialization
+	void Start () {
+        objectListScript = GameObject.Find("Objects").GetComponent<ObjectList>();
+
+        objectListArray = objectListScript.getObjectArray();
+
+        foreach (GameObject i in objectListArray)
+        {
+            Debug.Log(i);
+        }
+	}
+	
+	
+}
---
+++ LoremIpsum/Assets/Scripts/ObjectList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectList : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] objectArray = new GameObject[10];
+
+    public GameObject[] getObjectArray()
+    {
+        return objectArray;
+    }
+
+}
