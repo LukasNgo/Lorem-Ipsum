3ae3f567e717c9c164d85204c80615ce761c7b16
Lukas Ngo <32539108+LukasNgo@users.noreply.github.com>
2018-03-05 15:19:45 +0000

    test2

---
+++ LoremIpsum/Assets/Scripts/ObjectsMenuToggle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsMenuToggle : MonoBehaviour {
+
+    public GameObject menu;
+
+    private bool menuState = false;
+
+    public void ChangeState()
+    {
+        menuState = !menuState;
+        menu.SetActive(menuState);
+    }
+}
