290426de5cac0d1576f7295c18f8b61b3e13c1b3
(s) Aneurin Hodgson-Roch <ahodgson-roch>
2018-03-06 11:24:57 +0000

    Object Clipping
    Now Works

--- LoremIpsum/Assets/Scripts/ObjectInObjectDetection.cs
+++ LoremIpsum/Assets/Scripts/ObjectInObjectDetection.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class ObjectInObjectDetection : MonoBehaviour {
 
     public PlaceObjects placeObject;
+    public float timer = 0.3f;
+
+    private bool m_clip = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == placeObject.objectTag)
         {
             placeObject.canPlace = false;
-        }
-    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == placeObject.objectTag)
-        {
-            placeObject.canPlace = false;
+            StartCoroutine(Clipping());
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == placeObject.objectTag)
+        else if (m_clip == false)
         {
             placeObject.canPlace = true;
         }
+    }
+
+    private IEnumerator Clipping()
+    {
+        m_clip = true;
+
+        yield return new WaitForSeconds(timer);
+
+        m_clip = false;
     }
 
 }
