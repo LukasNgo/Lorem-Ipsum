1be8ee2574146856da9158cdcd6d9862c83e08b3
LukasNgo <32539108+LukasNgo@users.noreply.github.com>
2018-03-18 20:15:09 +0000

    ui + scripts
    created UI to show text on the screen while hovering the laser over an object. text shows some information about the selected object. SetObjectInfo script need to be placed onto a prefab and filled with text

---
+++ LoremIpsum/Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+
+public class ObjectInfo : MonoBehaviour
+{
+    [SerializeField]
+    private VRTK_BasePointerRenderer pointerRenderer;
+    [SerializeField]
+    private GameObject objectInfoUI;
+    [SerializeField]
+    private Text objectInfoText;
+
+    private GameObject selectedObject = null;
+
+    private void Update ()
+    {
+        if (pointerRenderer.IsVisible() && pointerRenderer.IsValidCollision() && pointerRenderer.GetDestinationHit().collider.CompareTag("SelectableObjects"))
+        {
+            selectedObject = pointerRenderer.GetDestinationHit().collider.gameObject;
+            objectInfoUI.SetActive(true);
+            ShowInfo();
+        }
+        else
+        {
+            selectedObject = null;
+            objectInfoUI.SetActive(false);
+        }
+    }
+
+    private void ShowInfo()
+    {
+        if (selectedObject != null)
+        {
+            objectInfoText.text = selectedObject.GetComponent<SetObjectInfo>().GetName() + "\n\n" + 
+                selectedObject.GetComponent<SetObjectInfo>().GetInfo();
+        }
+        else
+        {
+            objectInfoText.text = "";
+        }
+    }
+}
---
+++ LoremIpsum/Assets/Scripts/SetObjectInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObjectInfo : MonoBehaviour {
+
+    [SerializeField]
+    private string ObjectName, ObjectInfo;
+
+    public string GetName()
+    {
+        return ObjectName;
+    }
+    public string GetInfo()
+    {
+        return ObjectInfo;
+    }
+}
