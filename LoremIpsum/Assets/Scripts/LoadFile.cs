f58e0f810020621424217da9c5af5377369d66c9
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-04-01 19:45:02 +0100

    Created save script
    Need to add a menu plus input keyboard.

---
+++ LoremIpsum/Assets/Scripts/LoadFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class LoadFile : MonoBehaviour {
+
+    private string[] m_fileContent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
