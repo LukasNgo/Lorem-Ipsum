96843cec58ef201bf34d51e2bd96a246410509ac
DESKTOP-3PT05KH\Lukas <lukas.ngo@students.plymouth.ac.uk>
2018-04-13 15:03:00 +0100

    script to save strings
    added a script that saves all gameobjects tagged as selectableobject and saves their name, position and rotation into a list of strings.

---
+++ LoremIpsum/Assets/Scripts/GameobjectsToString.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameobjectsToString : MonoBehaviour {
+
+    public GameObject[] m_objectsToSave;
+    public List<string> m_returnString;
+
+	public List<string> ConvertGameObjects(){
+
+		m_objectsToSave = GameObject.FindGameObjectsWithTag("SelectableObjects");
+
+        if (m_objectsToSave != null)
+        {
+            foreach (GameObject saveObject in m_objectsToSave)
+            {
+                m_returnString.Add(saveObject.ToString());
+                Vector3 _tempVector = saveObject.transform.position;
+                m_returnString.Add(_tempVector.ToString("G4"));
+                m_returnString.Add(saveObject.transform.rotation.ToString("G4"));
+            }
+        }
+        return m_returnString;
+	}
+}
