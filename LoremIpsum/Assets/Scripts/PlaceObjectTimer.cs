35bf398ba00539d2c5f147dde389f841361d48e5
Lukas Ngo <32539108+LukasNgo@users.noreply.github.com>
2018-05-02 15:38:02 +0100

    bug fix
    fixed bug where object is being placed after closing the save or load menu.

---
+++ LoremIpsum/Assets/Scripts/PlaceObjectTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjectTimer : MonoBehaviour {
+
+    public PlaceObjects placeObjects;
+
+    public void openMenu()
+    {
+        placeObjects.canPlace = false;
+    }
+
+    public void closeMenu()
+    {
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1f);
+        placeObjects.canPlace = true;
+    }
+}
